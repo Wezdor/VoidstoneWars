db1e180770b25c2e19887495232ff54099b2e36c
Matic Leva <vertical3life@gmail.com>
2020-03-10 06:59:37 +0100

    Update create-tiles.js to display fancy progress bar

--- themes/dnd/create-tiles/create-tiles.js
+++ themes/dnd/create-tiles/create-tiles.js
@@ -1,40 +1,44 @@
 const Jimp = require('jimp');
-const {green, blue, magenta} = require('chalk');
+const {blue, magenta, red} = require('chalk');
 const {resolve} = require('path');
 const {mapSeries} = require("bluebird");
+const {SingleBar, Presets} = require('cli-progress');
 
 const config = [
-    // {
-    //     source: 'tessalonia.png',
-    //     zoom: 2,
-    //     scale: 4,
-    // },
     {
-        source: 'tessalonia.png',
+        source: '2020 - 6.png',
+        zoom: 2,
+        scale: 4,
+    },
+    {
+        source: '2020 - 6.png',
         zoom: 1,
         scale: 2,
     },
     {
-        source: 'tessalonia.png',
+        source: '2020 - 6.png',
         zoom: 0,
         scale: 1,
     },
     {
-        source: 'tessalonia.png',
+        source: '2020 - 6.png',
         zoom: -1,
         scale: 0.5,
     },
     {
-        source: 'tessalonia.png',
+        source: '2020 - 6.png',
         zoom: -2,
         scale: 0.25,
     },
 ];
 
 async function tiles(config) {
-    console.log(magenta(' _____            _               _         _   _ _                                        _             \r\n\/__   \\___   ___ | | ____ _ _ __ (_) __ _  | |_(_) | ___    __ _  ___ _ __   ___ _ __ __ _| |_ ___  _ __ \r\n  \/ \/\\\/ _ \\ \/ _ \\| |\/ \/ _` | \'_ \\| |\/ _` | | __| | |\/ _ \\  \/ _` |\/ _ \\ \'_ \\ \/ _ \\ \'__\/ _` | __\/ _ \\| \'__|\r\n \/ \/ | (_) | (_) |   < (_| | | | | | (_| | | |_| | |  __\/ | (_| |  __\/ | | |  __\/ | | (_| | || (_) | |   \r\n \\\/   \\___\/ \\___\/|_|\\_\\__,_|_| |_|_|\\__,_|  \\__|_|_|\\___|  \\__, |\\___|_| |_|\\___|_|  \\__,_|\\__\\___\/|_|   \r\n                                                           |___\/                                         '))
+    console.log(magenta(' _____                   _             _         _   _ _                                        _             \n|_   _|__  ___ ___  __ _| | ___  _ __ (_) __ _  | |_(_) | ___    __ _  ___ _ __   ___ _ __ __ _| |_ ___  _ __ \n  | |/ _ \\/ __/ __|/ _` | |/ _ \\| \'_ \\| |/ _` | | __| | |/ _ \\  / _` |/ _ \\ \'_ \\ / _ \\ \'__/ _` | __/ _ \\| \'__|\n  | |  __/\\__ \\__ \\ (_| | | (_) | | | | | (_| | | |_| | |  __/ | (_| |  __/ | | |  __/ | | (_| | || (_) | |   \n  |_|\\___||___/___/\\__,_|_|\\___/|_| |_|_|\\__,_|  \\__|_|_|\\___|  \\__, |\\___|_| |_|\\___|_|  \\__,_|\\__\\___/|_|   \n                                                                |___/                                         \n'));
     return mapSeries(config, async (level) => {
-        console.log(blue(`Starting zoom level ${level.zoom} at scale ${level.scale}`));
+        console.log(blue(`Loading image for level ${level.zoom} at scale ${level.scale}`));
+        const progressBar = new SingleBar({
+            format: '{bar} {percentage}% | {value}/{total}'
+        }, Presets.shades_classic);
         const image = await Jimp.read(resolve(__dirname, level.source));
 
         const scaled = await image
@@ -43,7 +47,10 @@ async function tiles(config) {
 
         const {width, height} = scaled.bitmap;
         const size = 256;
-        console.log(blue(`Loaded image with size ${width}x${height} (${Math.floor(width / size)}x${Math.floor(height / size)} tiles):`));
+        console.log(blue(`Splicing image with size ${width}x${height} (${Math.floor(width / size)}x${Math.floor(height / size)} tiles):`));
+        progressBar.start(Math.floor(width / size) * Math.floor(height / size), 0);
+        if(!process.stdout.isTTY)
+            console.log(red('Run this in terminal if you wish to see nice progress bar.'));
 
         for (let x = 0; x < width; x += size)
             for (let y = 0; y < height; y += size) {
@@ -57,8 +64,10 @@ async function tiles(config) {
                 const filename = `${level.zoom}/${x / size}/${y / size}.png`;
 
                 await tile.writeAsync(resolve(__dirname, '..', 'source', 'tiles', filename));
-                console.log(green(`Wrote tile ${level.zoom}/${x / size}/${y / size}.png `) + blue(`(${level.zoom}/${Math.floor(width / size)}/${Math.floor(height / size)})`))
+                progressBar.update(x / size * Math.floor(height / size) + y / size);
             }
+        progressBar.update(Math.floor(width / size) * Math.floor(height / size));
+        progressBar.stop();
     });
 }
 
